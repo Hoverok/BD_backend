319b572bed435af161d7ff72ff2a3e9fba1c5c93
user.email <pavelchi91@gmail.com>
2022-05-01 14:44:09 +0300

    DELETE on programs/programId checks if program has any exercises and if there are, deletes the comments before deleting the program, returns deleted program.

--- routes/programRouter.js
+++ routes/programRouter.js
@@ -5,6 +5,7 @@ var authenticate = require('../authenticate');
 const cors = require('./cors');
 
 const Programs = require('../models/programs');
+const Exercises = require('../models/exercises');
 
 const programRouter = express.Router();
 
@@ -46,8 +47,8 @@ programRouter.route('/')
             }, (err) => next(err))
             .catch((err) => next(err));
     });
-    
-    programRouter.route('/:programId')
+
+programRouter.route('/:programId')
     .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
     .get((req, res, next) => {
         Programs.findById(req.params.programId)
@@ -75,14 +76,69 @@ programRouter.route('/')
             .catch((err) => next(err));
     })
     .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-        Programs.findByIdAndRemove(req.params.programId)
-            .then((resp) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(resp);
+        Exercises.findOne({ program: req.params.programId })
+            .then((exercise) => {
+                if (exercise) {
+                    Exercises.deleteMany({ program: req.params.programId })
+                        .then((resp) => {
+                            Programs.findByIdAndRemove(req.params.programId)
+                                .then((resp) => {
+                                    res.statusCode = 200;
+                                    res.setHeader('Content-Type', 'application/json');
+                                    res.json(resp);
+                                })
+                        }, (err) => next(err))
+                        .catch((err) => next(err));
+                }
+                else {
+                    Programs.findByIdAndRemove(req.params.programId)
+                        .then((resp) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(resp);
+                        }, (err) => next(err))
+                        .catch((err) => next(err));
+                }
             }, (err) => next(err))
             .catch((err) => next(err));
+
+        // Exercises.deleteMany({ program: req.params.programId })
+        //     .then((resp) => {
+        //         Programs.findByIdAndRemove(req.params.programId)
+        //             .then((resp) => {
+        //                 res.statusCode = 200;
+        //                 res.setHeader('Content-Type', 'application/json');
+        //                 res.json(resp);
+        //             })
+        //     }, (err) => next(err))
+        //     .catch((err) => next(err));
+
     });
-    
+
+// .then((favorite) => {
+//     if (favorite) {
+//         if (favorite.dishes.indexOf(req.params.dishId) < 0) {
+//             favorite.dishes.push(req.body);
+//             favorite.save()
+//                 .then((favorite) => {
+//                     Favorites.findById(favorite._id)
+//                         .populate('user')
+//                         .populate('dishes')
+//                         .then((favorite) => {
+//                             res.statusCode = 200;
+//                             res.setHeader('Content-Type', 'application/json');
+//                             res.json(favorite);
+//                         })
+//                 })
+//                 .catch((err) => {
+//                     return next(err);
+//                 })
+//         }
+//         else {
+//             err = new Error('Dish id :' + req.params.dishId + ' already in favorites');
+//             err.status = 422;
+//             return next(err);
+//         }
+//     }
 
 module.exports = programRouter;
